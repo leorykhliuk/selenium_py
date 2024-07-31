a422521a4be97cb18bb10ca64c445579ee74d522
Rykhliuk <rykhliuk@Rykhliuks-MacBook-Pro.local>
2024-07-30 21:16:48 -0400

    Selenium_first_test_Check_page_title

---
+++ tests/test_first_test.py
@@ -0,0 +1,23 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import pytest
+
+@pytest.fixture
+def test_title():
+        driver = webdriver.Firefox()
+
+        driver.get('https://www.saucedemo.com/')
+
+        driver.implicitly_wait(0.8)
+        driver.find_element(By.ID, 'user-name').send_keys('standard_user')
+        driver.find_element(By.CSS_SELECTOR, 'input[type="password"]').send_keys('secret_sauce')
+        driver.find_element(By.CLASS_NAME, 'submit-button').click()
+
+        driver.implicitly_wait(1000)
+        print(driver.title)
+
+        assert driver.title == "Swag Labs"
+       
+        driver
+        driver.quit()
